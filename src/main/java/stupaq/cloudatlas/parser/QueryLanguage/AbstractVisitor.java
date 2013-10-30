57707a0e9b765498bacdddfb4dc321d86527d813
Mateusz Machalica <mateuszmachalica@gmail.com>
2013-10-30 21:01:35 +0100

    Generated parser.

---
+++ src/main/java/stupaq/cloudatlas/parser/QueryLanguage/AbstractVisitor.java
@@ -0,0 +1,136 @@
+package stupaq.cloudatlas.parser.QueryLanguage;
+import stupaq.cloudatlas.parser.QueryLanguage.Absyn.*;
+/** BNFC-Generated Abstract Visitor */
+public class AbstractVisitor<R,A> implements AllVisitor<R,A> {
+/* XProgram */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.Program p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XProgram p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XStatement */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.Statement p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XStatement p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XWhereClause */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.WhereClause p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.WhereClauseEmpty p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XWhereClause p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XOrderClause */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderClause p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderClauseEmpty p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XOrderClause p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XOrderItem */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderItemCond p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XOrderItem p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XOrderOption */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderOptionAsc p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderOptionDesc p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderOptionEmpty p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XOrderOption p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XNullsOption */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.NullsOptionFirst p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.NullsOptionLast p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.NullsOptionEmpty p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XNullsOption p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XSelectItem */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.SelectItem p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.SelectItemAs p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XSelectItem p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XExpression */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprOr p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprAnd p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprNot p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRegex p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRel p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithExprAdd p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithExprMultiply p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithExprNeg p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprVar p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprCall p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprString p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprTrue p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprInt p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprDouble p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprBraces p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprBrackets p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprAngle p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprStmt p, A arg) { return visitDefault(p, arg); }
+
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XExpression p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XArithOpAdd */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpAdd p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpSubstract p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XArithOpAdd p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XArithOpMultiply */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpMultiply p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpDivide p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpModulo p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XArithOpMultiply p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XRelOp */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpEqual p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpNotEqual p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpGreater p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpGreaterEqual p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesser p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesserEqual p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XRelOp p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XBoolConst */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolConstTrue p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolConstFalse p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XBoolConst p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XExpressionNGT */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprOrNGT p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprAndNGT p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprNotNGT p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRegexNGT p, A arg) { return visitDefault(p, arg); }
+
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRelNGT p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ExpressionNGT p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XExpressionNGT p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+/* XRelOpNGT */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpEqualNGT p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpNotEqualNGT p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpGreaterEqualNGT p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesserNGT p, A arg) { return visitDefault(p, arg); }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesserEqualNGT p, A arg) { return visitDefault(p, arg); }
+    public R visitDefault(stupaq.cloudatlas.parser.QueryLanguage.Absyn.XRelOpNGT p, A arg) {
+      throw new IllegalArgumentException(this.getClass().getName() + ": " + p);
+    }
+
+}
---
+++ src/main/java/stupaq/cloudatlas/parser/QueryLanguage/FoldVisitor.java
@@ -0,0 +1,323 @@
+package stupaq.cloudatlas.parser.QueryLanguage;
+
+import stupaq.cloudatlas.parser.QueryLanguage.Absyn.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+/** BNFC-Generated Fold Visitor */
+public abstract class FoldVisitor<R,A> implements AllVisitor<R,A> {
+    public abstract R leaf(A arg);
+    public abstract R combine(R x, R y, A arg);
+
+/* XProgram */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.Program p, A arg) {
+      R r = leaf(arg);
+      for (XStatement x : p.listxstatement_) {
+        r = combine(x.accept(this,arg), r, arg);
+      }
+      return r;
+    }
+
+/* XStatement */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.Statement p, A arg) {
+      R r = leaf(arg);
+      for (XSelectItem x : p.listxselectitem_) {
+        r = combine(x.accept(this,arg), r, arg);
+      }
+      r = combine(p.xwhereclause_.accept(this, arg), r, arg);
+      r = combine(p.xorderclause_.accept(this, arg), r, arg);
+      return r;
+    }
+
+/* XWhereClause */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.WhereClause p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.WhereClauseEmpty p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XOrderClause */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderClause p, A arg) {
+      R r = leaf(arg);
+      for (XOrderItem x : p.listxorderitem_) {
+        r = combine(x.accept(this,arg), r, arg);
+      }
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderClauseEmpty p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XOrderItem */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderItemCond p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      r = combine(p.xorderoption_.accept(this, arg), r, arg);
+      r = combine(p.xnullsoption_.accept(this, arg), r, arg);
+      return r;
+    }
+
+/* XOrderOption */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderOptionAsc p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderOptionDesc p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.OrderOptionEmpty p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XNullsOption */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.NullsOptionFirst p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.NullsOptionLast p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.NullsOptionEmpty p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XSelectItem */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.SelectItem p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.SelectItemAs p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+
+/* XExpression */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprOr p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_1.accept(this, arg), r, arg);
+      r = combine(p.xexpression_2.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprAnd p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_1.accept(this, arg), r, arg);
+      r = combine(p.xexpression_2.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprNot p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRegex p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRel p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_1.accept(this, arg), r, arg);
+      r = combine(p.xrelop_.accept(this, arg), r, arg);
+      r = combine(p.xexpression_2.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithExprAdd p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_1.accept(this, arg), r, arg);
+      r = combine(p.xarithopadd_.accept(this, arg), r, arg);
+      r = combine(p.xexpression_2.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithExprMultiply p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_1.accept(this, arg), r, arg);
+      r = combine(p.xarithopmultiply_.accept(this, arg), r, arg);
+      r = combine(p.xexpression_2.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithExprNeg p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprVar p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprCall p, A arg) {
+      R r = leaf(arg);
+      for (XExpression x : p.listxexpression_) {
+        r = combine(x.accept(this,arg), r, arg);
+      }
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprString p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprTrue p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xboolconst_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprInt p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprDouble p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprBraces p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprBrackets p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprAngle p, A arg) {
+      R r = leaf(arg);
+      for (XExpressionNGT x : p.listxexpressionngt_) {
+        r = combine(x.accept(this,arg), r, arg);
+      }
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BasicExprStmt p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xstatement_.accept(this, arg), r, arg);
+      return r;
+    }
+
+/* XArithOpAdd */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpAdd p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpSubstract p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XArithOpMultiply */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpMultiply p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpDivide p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ArithOpModulo p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XRelOp */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpEqual p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpNotEqual p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpGreater p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpGreaterEqual p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesser p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesserEqual p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XBoolConst */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolConstTrue p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolConstFalse p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+/* XExpressionNGT */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprOrNGT p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpressionngt_1.accept(this, arg), r, arg);
+      r = combine(p.xexpressionngt_2.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprAndNGT p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpressionngt_1.accept(this, arg), r, arg);
+      r = combine(p.xexpressionngt_2.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.CondExprNotNGT p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpressionngt_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRegexNGT p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpressionngt_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.BoolExprRelNGT p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpressionngt_.accept(this, arg), r, arg);
+      r = combine(p.xrelopngt_.accept(this, arg), r, arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.ExpressionNGT p, A arg) {
+      R r = leaf(arg);
+      r = combine(p.xexpression_.accept(this, arg), r, arg);
+      return r;
+    }
+
+/* XRelOpNGT */
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpEqualNGT p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpNotEqualNGT p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpGreaterEqualNGT p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesserNGT p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+    public R visit(stupaq.cloudatlas.parser.QueryLanguage.Absyn.RelOpLesserEqualNGT p, A arg) {
+      R r = leaf(arg);
+      return r;
+    }
+
+
+}
