1ce6d4f7f6a36b77047dd2ed188a0fa41622726d
Mateusz Machalica <mateuszmachalica@gmail.com>
2013-11-14 22:24:12 +0100

    Null semantics tests for RList.

--- src/test/java/stupaq/cloudatlas/interpreter/types/RListTest.java
+++ src/test/java/stupaq/cloudatlas/interpreter/types/RListTest.java
@@ -3,9 +3,12 @@ package stupaq.cloudatlas.interpreter.types;
 import org.junit.Test;
 
 import stupaq.cloudatlas.interpreter.errors.EvaluationException;
+import stupaq.cloudatlas.interpreter.semantics.AggregatingValue.AggregatingValueDefault;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 import static stupaq.cloudatlas.interpreter.types.SemanticValueTestUtils.BinOp;
+import static stupaq.cloudatlas.interpreter.types.SemanticValueTestUtils.C;
 import static stupaq.cloudatlas.interpreter.types.SemanticValueTestUtils.Int;
 import static stupaq.cloudatlas.interpreter.types.SemanticValueTestUtils.L;
 import static stupaq.cloudatlas.interpreter.types.SemanticValueTestUtils.S;
@@ -16,6 +19,8 @@ public class RListTest {
   public void testSemanticsMap() {
     // map
     assertEquals(L(Int(-2), Int(-3)), L(Int(2), Int(3)).map(UnOp()));
+    // map null
+    assertEquals(L(null, Int(-3)), L(null, Int(3)).map(UnOp()));
   }
 
   @Test(expected = EvaluationException.class)
@@ -24,5 +29,15 @@ public class RListTest {
     assertEquals(L(Int(3), Int(4)), L(Int(5), Int(3)).zip(L(Int(2), Int(-1)), BinOp()));
     assertEquals(L(Int(3), Int(1)), L(Int(5), Int(3)).zip(S(Int(2)), BinOp()));
     assertEquals(L(Int(-3), Int(-1)), S(Int(2)).zip(L(Int(5), Int(3)), BinOp()));
+    // zip null
+    assertEquals(L(null, null), L(null, Int(1)).zip(L(null, Int(1)), BinOp()));
+    assertEquals(L(null, null), L(null, Int(1)).zip(S(Int()), BinOp()));
+    assertEquals(L(null, null), S(Int()).zip(L(null, Int(3)), BinOp()));
+  }
+
+  @Test
+  public void testAggregators() {
+    assertEquals(C(Int(2)).aggregate().getClass(), L(Int(2)).aggregate().getClass());
+    assertNotSame(AggregatingValueDefault.class, C(Int(2)).aggregate().getClass());
   }
 }
