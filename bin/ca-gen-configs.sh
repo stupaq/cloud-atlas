d4a5ed3dfb2baa8e83d743a00bc169c941ffdcb1
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-01-08 16:01:13 +0100

    Labs configs.

--- bin/ca-gen-configs.sh
+++ bin/ca-gen-configs.sh
@@ -13,26 +13,26 @@ gen_queries="$SKELETONS_DIR/CAQueriesInstaller.sh"
 port_seed="$PORT_RANGE_PREFIX"
 
 # /pjwstk/whatever01
-$gen_agent /pjwstk/whatever01 "localhost" "${port_seed}01"
-$gen_client /pjwstk/whatever01 "localhost:${port_seed}02"
+$gen_agent /pjwstk/whatever01 "violet01" "${port_seed}01"
+$gen_client /pjwstk/whatever01 "violet02:${port_seed}02"
 $gen_queries /pjwstk/whatever01
 
 # /pjwstk/whatever02
-$gen_agent /pjwstk/whatever02 "localhost" "${port_seed}02"
-$gen_client /pjwstk/whatever02 "localhost:${port_seed}07"
+$gen_agent /pjwstk/whatever02 "violet02" "${port_seed}02"
+$gen_client /pjwstk/whatever02 "violet05:${port_seed}07"
 $gen_queries /pjwstk/whatever02
 
 # /uw/khaki13
-$gen_agent /uw/khaki13 "localhost" "${port_seed}13"
-$gen_client /uw/khaki13 "localhost:${port_seed}31"
+$gen_agent /uw/khaki13 "violet03" "${port_seed}13"
+$gen_client /uw/khaki13 "violet04:${port_seed}31"
 $gen_queries /uw/khaki13
 
 # /uw/khaki31
-$gen_agent /uw/khaki31 "localhost" "${port_seed}31"
-$gen_client /uw/khaki31 "localhost:${port_seed}07"
+$gen_agent /uw/khaki31 "violet04" "${port_seed}31"
+$gen_client /uw/khaki31 "violet05:${port_seed}07"
 $gen_queries /uw/khaki31
 
 # /uw/violet07
-$gen_agent /uw/violet07 "localhost" "${port_seed}07"
-$gen_client /uw/violet07 "localhost:${port_seed}02"
+$gen_agent /uw/violet07 "violet05" "${port_seed}07"
+$gen_client /uw/violet07 "violet02:${port_seed}02"
 $gen_queries /uw/violet07
